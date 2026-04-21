720e657d21414374accc1408d90fc73e40b75efd
Michael Teschner <120539187+BlazeTechDev@users.noreply.github.com>
2026-04-21 10:37:56 -0500

    ui size change

--- WebDogfight.js
+++ WebDogfight.js
@@ -68,7 +68,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\micha\AppData\Local\Temp\tmpmyigs0x0.js
+// include: C:\Users\micha\AppData\Local\Temp\tmph2so37m_.js
 if (!Module["expectedDataFileDownloads"]) Module["expectedDataFileDownloads"] = 0;
 
 Module["expectedDataFileDownloads"]++;
@@ -343,23 +343,23 @@ Module["expectedDataFileDownloads"]++;
   });
 })();
 
-// end include: C:\Users\micha\AppData\Local\Temp\tmpmyigs0x0.js
-// include: C:\Users\micha\AppData\Local\Temp\tmp_wcufn77.js
+// end include: C:\Users\micha\AppData\Local\Temp\tmph2so37m_.js
+// include: C:\Users\micha\AppData\Local\Temp\tmpc3f175az.js
 // All the pre-js content up to here must remain later on, we need to run
 // it.
 if ((typeof ENVIRONMENT_IS_WASM_WORKER != "undefined" && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != "undefined" && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != "undefined" && ENVIRONMENT_IS_AUDIO_WORKLET)) Module["preRun"] = [];
 
 var necessaryPreJSTasks = Module["preRun"].slice();
 
-// end include: C:\Users\micha\AppData\Local\Temp\tmp_wcufn77.js
-// include: C:\Users\micha\AppData\Local\Temp\tmpyndnist4.js
+// end include: C:\Users\micha\AppData\Local\Temp\tmpc3f175az.js
+// include: C:\Users\micha\AppData\Local\Temp\tmpzvld99cj.js
 if (!Module["preRun"]) throw "Module.preRun should exist because file support used it; did a pre-js delete it?";
 
 necessaryPreJSTasks.forEach(task => {
   if (Module["preRun"].indexOf(task) < 0) throw "All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?";
 });
 
-// end include: C:\Users\micha\AppData\Local\Temp\tmpyndnist4.js
+// end include: C:\Users\micha\AppData\Local\Temp\tmpzvld99cj.js
 var arguments_ = [];
 
 var thisProgram = "./this.program";
